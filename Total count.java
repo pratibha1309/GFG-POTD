6e126d0fe136589b653470d44f1c3fcca13e6183
Pratibha <124805883+pratibha1309@users.noreply.github.com>
2024-09-29 20:53:41 +0530

    Add files via upload

---
+++ Total count.java
@@ -0,0 +1,13 @@
+class Solution {
+    int totalCount(int k, int[] arr) {
+        int total = 0;
+        
+        for (int num : arr) {
+            // Calculate how many parts are needed for num
+            total += (num + k - 1) / k; // This is equivalent to Math.ceil(num / k)
+        }
+        
+        return total;
+    }
+}
+//gfg
